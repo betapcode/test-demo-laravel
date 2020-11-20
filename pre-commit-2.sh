0a4c80a733c3e7b26608b87e54b0435f75f4817f
Phạm Ngọc Minh <betapcode@gmail.com>
2020-11-20 13:32:44 +0700

    Update pre-commit-2.sh

--- pre-commit-2.sh
+++ pre-commit-2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Running Code Sniffer..."
-./vendor/bin/phpcs --standard=PSR2 --encoding=utf-8 -n -p .
+./vendor/bin/phpcs --ignore=vendor,bootstrap/cache/,storage --standard=PSR2 --encoding=utf-8 -n -p .
 if [ $? != 0 ]
 then
   echo "Fix the error before commit."
