a6d74e5a38653351ca0b3d10b62a82c76892abc4
Phạm Ngọc Minh <betapcode@gmail.com>
2020-11-20 18:29:11 +0700

    Update pre-commit-2.sh

--- pre-commit-2.sh
+++ pre-commit-2.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+##### Constants
+########################################################################
+PHPCS="php phpcs.phar"
+PHPCBF="php phpcbf.phar"
+
+##### Init
+########################################################################
+if [ ! -f "$PHPCS" ]; then
+    wget https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar
+fi
+if [ ! -f "$PHPCBF" ]; then
+    wget https://squizlabs.github.io/PHP_CodeSniffer/phpcbf.phar
+fi
+
 echo "Running Code Sniffer..."
-./vendor/bin/phpcs --ignore=vendor,bootstrap/cache/,storage --standard=PSR2 --encoding=utf-8 -n -p app/Http/Controllers/Auth/LoginController.php
+$PHPCS --ignore=vendor,bootstrap/cache/,storage --standard=PSR2 --encoding=utf-8 -n -p app/Http/Controllers/Auth/LoginController.php
 if [ $? != 0 ]
 then
   echo "Fix the error before commit."
